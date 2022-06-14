e9de05347d47357e2725cadb5bc7c36bfe364330
Mihir <mihirkantho@gmail.com>
2022-06-15 04:32:43 +0600

    just add a comoponents

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <><h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1><Header></Header></>
   );
 }
 
